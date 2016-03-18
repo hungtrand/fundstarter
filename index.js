64998b6c718c42657cf05708761ff9fd7ef823de
Hung Tran <hung_duy_tran@yahoo.com>
2016-03-17 20:43:16 -0700

    put port number into variable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 
+var port = process.env.PORT || 80;
 
 /******************************* PART 1A ***********************************/
 
@@ -15,7 +16,7 @@ var listener_a = function(req, res) {
 }
 
 var server_a = http.createServer(listener_a);
-//server_a.listen('80');
+//server_a.listen(port);
 /************************** END of PART 1A ********************************/
 
 
@@ -38,7 +39,7 @@ var listener_b = function(req, res) {
 }
 
 var server_b = http.createServer(listener_b);
-//server_b.listen('80');
+//server_b.listen(port);
 /**************************** END of PART 1B *******************************/
 
 /*************************** PART 2 - fs.open() ****************************/
@@ -73,6 +74,6 @@ var listener_2_open = function(req, res) {
 }
 
 var server_2_open = http.createServer(listener_2_open);
-server_2_open.listen("80");
+server_2_open.listen(port);
 /*************************** PART 2 - end fs.open() ***********************/
 
