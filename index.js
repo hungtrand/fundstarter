6859ed9693049213f89f7b107a6a68ef054fbb1d
Hung Tran <hung_duy_tran@yahoo.com>
2016-03-17 20:15:00 -0700

    part 1 b

--- index.js
+++ index.js
@@ -18,3 +18,27 @@ var server_a = http.createServer(listener_a);
 server_a.listen('8080');
 /************************** END of PART 1A ********************************/
 
+
+
+/*************************** PART 1B *************************************/
+var readHTML = function(callback) {
+    fs.readFile("./index.html", "utf8", function(err, content) {
+	if (err) return callback(err);
+	
+	callback(null, content);
+    });
+}
+
+var listener_b = function(req, res) {
+    readHTML(function(err, content) {
+	res.writeHead(200);
+	
+	res.end(content);
+    });
+}
+
+var server_b = http.createServer(listener_b);
+server_b.listen('8081');
+/**************************** END of PART 1B *******************************/
+
+
