5b86847f2d2d3cd06cf98a2d6eb3810c035232dc
Hung Tran <hung_duy_tran@yahoo.com>
2016-03-17 20:15:42 -0700

    part 2

--- index.js
+++ index.js
@@ -41,4 +41,38 @@ var server_b = http.createServer(listener_b);
 server_b.listen('8081');
 /**************************** END of PART 1B *******************************/
 
+/*************************** PART 2 - fs.open() ****************************/
+var openHTML = function(callback) {
+    var file = "./index.html";
+    fs.exists(file, function(exists) {
+	if (!exists) return callback("File not found:" + file);
+
+	fs.stat(file, function(statErr, stats) {
+	    if (statErr) return  callback(statErr);
+
+	    fs.open("./index.html", "r", function(openErr, fd) {
+		if (openErr) return callback(openErr);
+		
+		var buff = new Buffer(stats.size);
+
+		fs.read(fd, buff, 0, buff.length, null, function(readErr, bytesRead, readBuff) {
+		    var content = readBuff.toString("utf8", 0, readBuff.length);
+		    callback(readErr, content);
+		    fs.close(fd);
+		});
+	    });
+	});
+    });
+}
+
+var listener_2_open = function(req, res) {
+    openHTML(function(err, content) {
+	res.writeHead(200);
+	res.end(content);
+    });
+}
+
+var server_2_open = http.createServer(listener_2_open);
+server_2_open.listen("8082");
+/*************************** PART 2 - end fs.open() ***********************/
 
